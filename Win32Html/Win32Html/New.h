c4c5f7c68410e5d12a323715b5818a12b755aab3
SonnAdolf <programeryu@163.com>
2017-04-05 11:33:28 +0800

    htmlayout demo deom operation.

--- Win32Html/Win32Html/New.h
+++ Win32Html/Win32Html/New.h
@@ -1,22 +1,113 @@
 #pragma once
-
 #include <htmlayout.h>
 #include "behaviors/notifications.h"
 #include <htmlayout_behavior.hpp>
 
 namespace NEW
 {
+
 	#define MAX_LOADSTRING 100
 	// 全局变量:
 	HINSTANCE hInst;								// 当前实例
 	TCHAR szTitle[MAX_LOADSTRING];					// 标题栏文本
 	TCHAR szWindowClass[MAX_LOADSTRING];			// 主窗口类名
+	HWND hwnd;
 
 	// 此代码模块中包含的函数的前向声明:
 	ATOM				MyRegisterClass(HINSTANCE hInstance);
 	BOOL				InitInstance(HINSTANCE, int);
 	LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
 	INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
+
+
+		//************************************
+	// 作      者:	sonne
+	// 函  数  名:  InitList
+	// 功      能:  窗口初始化函数
+	// 完  整  名:  MANAGE::InitList
+	// 访  问  权:  public 
+	// 返回值类型:  VOID
+	// 方法限定符: 	
+	//************************************
+	VOID InitList(HWND hwnd)
+	{
+		htmlayout::dom::element root;
+		htmlayout::dom::element	name;
+		root = htmlayout::dom::element::root_element(hwnd); 
+		name = root.get_element_by_id("NAME");
+		name.clear();
+		name.insert(htmlayout::dom::element::create("option", L"Bob"), 0);
+		name.insert(htmlayout::dom::element::create("option", L"Alice"), 1);
+		name.insert(htmlayout::dom::element::create("option", L"Peter"), 2);
+		SetWindowText(hwnd,"NAME LIST");
+	}
+
+	void OnButtonClick(HELEMENT button);
+
+	struct DOMEventsHandlerType: htmlayout::event_handler
+	{
+		DOMEventsHandlerType(): event_handler(0xFFFFFFFF) {}
+		virtual BOOL handle_event (HELEMENT he, BEHAVIOR_EVENT_PARAMS& params ) 
+		{ 
+			switch( params.cmd )
+			{
+			case BUTTON_CLICK:              NEW::OnButtonClick(params.heTarget); break;// click on button
+		    case BUTTON_PRESS:              break;// mouse down or key down in button
+		    case BUTTON_STATE_CHANGED:      break;
+			case EDIT_VALUE_CHANGING:       break;// before text change
+		    case EDIT_VALUE_CHANGED:        break;//after text change
+		    case SELECT_SELECTION_CHANGED:  break;// selection in <select> changed
+		    case SELECT_STATE_CHANGED:      break;// node in select expanded/collapsed, heTarget is the node
+            case POPUP_REQUEST: 
+                                        break;// request to show popup just received, 
+                                              //     here DOM of popup element can be modifed.
+		    case POPUP_READY:               break;// popup element has been measured and ready to be shown on screen,
+                                              //     here you can use functions like ScrollToView.
+            case POPUP_DISMISSED:           break;// popup element is closed,
+                                              //     here DOM of popup element can be modifed again - e.g. some items can be removed
+                                              //     to free memory.
+            case MENU_ITEM_ACTIVE:                // menu item activated by mouse hover or by keyboard
+				 break;
+            case MENU_ITEM_CLICK:                 // menu item click 
+				 break;
+
+
+				// "grey" event codes  - notfications from behaviors from this SDK 
+			case HYPERLINK_CLICK:           break;// hyperlink click
+			case TABLE_HEADER_CLICK:        break;// click on some cell in table header, 
+                                              //     target = the cell, 
+                                              //     reason = index of the cell (column number, 0..n)
+		    case TABLE_ROW_CLICK:           break;// click on data row in the table, target is the row
+                                              //     target = the row, 
+                                              //     reason = index of the row (fixed_rows..n)
+		    case TABLE_ROW_DBL_CLICK:       break;// mouse dbl click on data row in the table, target is the row
+                                              //     target = the row, 
+                                              //     reason = index of the row (fixed_rows..n)
+
+            case ELEMENT_COLLAPSED:         break;// element was collapsed, so far only behavior:tabs is sending these two to the panels
+            case ELEMENT_EXPANDED:          break;// element was expanded,
+			}
+			return FALSE; 
+		}
+	} DOMEventsHandler;
+
+	LRESULT CALLBACK HTMLayoutNotifyHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, LPVOID vParam)
+	{
+		// all HTMLayout notification are comming here.
+		NMHDR*  phdr = (NMHDR*)lParam;
+
+		switch(phdr->code)
+		{
+		case HLN_CREATE_CONTROL:    break; //return OnCreateControl((LPNMHL_CREATE_CONTROL) lParam);
+		case HLN_CONTROL_CREATED:   break; //return OnControlCreated((LPNMHL_CREATE_CONTROL) lParam);
+		case HLN_DESTROY_CONTROL:   break; //return OnDestroyControl((LPNMHL_DESTROY_CONTROL) lParam);
+		case HLN_LOAD_DATA:         break;
+		case HLN_DATA_LOADED:       break; //return OnDataLoaded((LPNMHL_DATA_LOADED)lParam);
+		case HLN_DOCUMENT_COMPLETE: break; //return OnDocumentComplete();
+		case HLN_ATTACH_BEHAVIOR:   break;
+		}
+		return 0;
+	}
 
     //
 	//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
@@ -47,9 +138,19 @@ namespace NEW
         switch (message)  
         { 
 		case WM_CREATE: 
+			// Normally HTMLayout sends its notifications
+            // to its parent. 
+            // In this particular case we are using callback function to receive and
+            // and handle notification. Don't bother the desktop window (parent of this window)
+            // by our notfications.
+            HTMLayoutSetCallback(hWnd,&HTMLayoutNotifyHandler,0);
+
+            // attach DOM events handler so we will be able to receive DOM events like BUTTON_CLICK, HYPERLINK_CLICK, etc.
+			htmlayout::attach_event_handler(hWnd, &DOMEventsHandler);
 			memset(wsz,0,sizeof(wsz));
 			MultiByteToWideChar(CP_ACP, 0, path, strlen(path)+1, wsz, sizeof(wsz)/sizeof(wsz[0]));
             HTMLayoutLoadFile(hWnd,wsz); //Hello.htm需要放在和exe同一目录,记得把dll也copy过去  
+			InitList(hWnd);
 			break;  
    
         case WM_COMMAND:  
@@ -108,6 +209,7 @@ namespace NEW
 		return RegisterClassEx(&wcex);
 	}
 
+
 	//
 	//   函数: InitInstance(HINSTANCE, int)
 	//
@@ -122,7 +224,6 @@ namespace NEW
 	//
 	BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, HWND hWndPar, TCHAR szWindowClass[])
 	{
-		HWND newHWnd;
 		hInst = hInstance; // 将实例句柄存储在全局变量中
 		htmlayout::dom::element root;
 		char szCurrPath[MAX_PATH];
@@ -130,18 +231,18 @@ namespace NEW
 
 		NewRegisterClass(hInst);
 
-		newHWnd = CreateWindow("hh", NULL, WS_OVERLAPPEDWINDOW,
+		hwnd = CreateWindow("hh", NULL, WS_OVERLAPPEDWINDOW,
 			CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, hWndPar, NULL, hInst, NULL);
 
 		EnableWindow(hWndPar,FALSE);
 
-	    if (!newHWnd)
+	    if (!hwnd)
 	    {
 			return FALSE;
 		}
 
-	    ShowWindow(newHWnd, 5);
-        UpdateWindow(newHWnd);
+	    ShowWindow(hwnd, 5);
+        UpdateWindow(hwnd);
 
 		MSG msg;
 	    HACCEL hAccelTable;
@@ -187,4 +288,50 @@ namespace NEW
 		return (INT_PTR)FALSE;
 	}
 
+	
+	//************************************
+	// 作      者:	sonne
+	// 函  数  名:  OnButtonClick
+	// 功      能:  按钮响应事件
+	// 完  整  名:  OnButtonClick
+	// 访  问  权:  public 
+	// 返回值类型:  VOID
+	// 方法限定符: 	
+	//************************************
+	void OnButtonClick(HELEMENT button)
+	{
+		htmlayout::dom::element cBut = button;
+		if (!wcscmp(cBut.get_attribute("id"),L"ACT"))
+		{
+			htmlayout::dom::element root;
+			htmlayout::dom::element	name;
+			root = htmlayout::dom::element::root_element(hwnd); 
+			name = root.get_element_by_id("NAME");
+			name.insert(htmlayout::dom::element::create("option", L"Whoami"), 0);
+		}
+		if (!wcscmp(cBut.get_attribute("id"),L"DEL"))
+		{
+			htmlayout::dom::element root;
+			htmlayout::dom::element	nameList;
+			root = htmlayout::dom::element::root_element(hwnd); 
+			nameList = root.get_element_by_id("NAME");
+			htmlayout::dom::element selName;
+			selName = nameList.find_first("option:checked");
+			selName.destroy();
+			SetWindowText(hwnd,"NAME LIST");
+		}
+		if (!wcscmp(cBut.get_attribute("id"),L"EDI"))
+		{
+			htmlayout::dom::element root;
+			htmlayout::dom::element	nameList;
+			root = htmlayout::dom::element::root_element(hwnd); 
+			nameList = root.get_element_by_id("NAME");
+			htmlayout::dom::element selName;
+			selName = nameList.find_first("option:checked");
+			selName.set_value("Iamwho");
+			SetWindowText(hwnd,"NAME LIST");
+		}
+	}
+
 }
+
